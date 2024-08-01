ce0e7b204c51102a29f16802c08e0b29ae5b94b2
donghoony <aru0504@naver.com>
2024-08-01 20:43:30 +0900

    init: Initialize project

---
+++ src/main/java/hoony/submodule/SubmoduleExampleApplication.java
@@ -0,0 +1,13 @@
+package hoony.submodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubmoduleExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubmoduleExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hoony/submodule/SubmoduleExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package hoony.submodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubmoduleExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
